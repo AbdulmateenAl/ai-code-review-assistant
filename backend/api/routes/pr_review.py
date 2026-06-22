a7e7e77fe27c6a40296bfb3150779cc8a8e44207
AbdulmateenAl <alabiabdulmateen94@gmail.com>
2026-06-22 01:33:26 +0100

    done with serviecs and route for pr agent

---
+++ backend/api/routes/pr_review.py
@@ -0,0 +1,114 @@
+import os
+
+import requests
+from fastapi import status, Response, Request, HTTPException
+from fastapi.responses import ORJSONResponse, PlainTextResponse
+from fastapi import APIRouter
+from starlette.responses import JSONResponse
+
+from api.services.pr_review_service import analyze_diff_with_gemini, pr_comment, fetch_repo_contents, \
+    analyze_repo_with_gemini, create_github_issues
+
+pr_review = APIRouter(prefix="/pr_review", tags=["PR-review-agent"])
+
+
+# @pr_review.get("/", summary="List PRs", status_code=status.HTTP_200_OK)
+# async def list_pr(request: Request,
+#                   response: Response,
+#                   owner: str,
+#                   repo: str,):
+#
+#     response = await get_pr(owner=owner, repo=repo)
+#     return ORJSONResponse(response.json())
+
+
+@pr_review.get('/post_pr_comment', summary="Fetch specific PR diff and post comment")
+async def fetch_pull_request_diff(
+        owner: str,
+        repo: str,
+        pr_number: int
+):
+
+    api_url = f'https://api.github.com/repos/{owner}/{repo}/pulls/{pr_number}'
+    headers = {
+        'Accept': 'application/vnd.github+json',
+        'Authorization': f'Bearer {os.getenv("GITHUB_PERSONAL_ACCESS_TOKEN")}'
+    }
+
+
+    pr_response = requests.get(api_url, headers)
+
+    if not pr_response.ok:
+        detail = {"message": pr_response.text}
+        raise HTTPException(status_code=pr_response.status_code, detail=detail)
+
+    pr_data = pr_response.json()
+
+
+    diff_url = f'https://api.github.com/repos/{owner}/{repo}/pulls/{pr_number}/files'
+    files_response = requests.get(diff_url, headers)
+
+    if not files_response.ok:
+        detail = {"message": files_response.text}
+        raise HTTPException(status_code=files_response.status_code, detail=detail)
+
+    files_data = files_response.json()
+
+
+    changes = []
+    for file in files_data:
+        change = {
+            "filename": file["filename"],
+            "status": file["status"],
+            "additions": file["additions"],
+            "deletions": file["deletions"],
+            "patch": file.get("patch", "")
+        }
+        changes.append(change)
+
+
+    formatted_diff = {
+        "pr_title": pr_data["title"],
+        "pr_description": pr_data["body"],
+        "changes": changes
+    }
+
+    analysis_result = await analyze_diff_with_gemini(formatted_diff)
+
+    if isinstance(analysis_result, HTTPException):
+        return JSONResponse(
+            content={"error": f"Analysis failed: {analysis_result.detail}"},
+            status_code=analysis_result.status_code
+        )
+
+    comment = await pr_comment(owner=owner, repo=repo, pr_number=pr_number, comment=analysis_result)
+
+
+    return JSONResponse(content={'message': 'PR review completed!', 'result': comment})
+
+
+
+#--------------ANALYZE REPOSITORY CONTENT TO POST ISSUES--------------#
+@pr_review.get('/analyze_repository', summary="Analyze repository")
+async def analyze_repository(owner: str, repo: str):
+    """Endpoint to analyze a repository and create issues"""
+    try:
+        files = await fetch_repo_contents(owner, repo)
+
+        if not files:
+            return {"message": "No files found or error fetching repository"}
+
+        issues = await analyze_repo_with_gemini(files, owner, repo)
+
+
+        created_issues = await create_github_issues(owner, repo, issues)
+
+        return JSONResponse(
+            content={
+                "message": f"Analysis complete. Created {len(created_issues)} issues.",
+                "issues": created_issues
+            }
+        )
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
---
+++ backend/main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI, Request, HTTPException, Response, status
+import uvicorn
+from fastapi.responses import ORJSONResponse
+from api.services.pr_review_service import fetch_pull_request_diff_with_app
+from api.routes import api_version_one
+import os
+
+app = FastAPI(
+    title="PR-review-agent",
+    description="A simple agent for PR reviews",
+    version="1.0",
+    default_response_class=ORJSONResponse,
+
+)
+
+app.include_router(api_version_one)
+
+@app.get("/", tags=["Home"])
+async def get_root(request: Request):
+    """Base endpoint"""
+    return "Welcome to PR-review-agent API",
+
+@app.post("/webhook")
+async def github_webhook(request: Request):
+    """Webhook to automatically review PRs when opened"""
+    try:
+        payload = await request.json()
+        raw = await request.body()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Invalid JSON body: {e}")
+
+    # Check if it's a PR opened or reopened event
+    if (payload.get('action') == 'opened' or payload.get('action') == 'reopened'  and  'pull_request' in payload):
+        owner = payload['repository']['owner']['login']
+        print(f'owner: {owner}')
+        repo_name = payload['repository']['name']
+        print(f'repo_name: {repo_name}')
+        pr_number = payload['pull_request']['number']
+        print(f'pr_number: {pr_number}')
+
+        try:
+            await fetch_pull_request_diff_with_app(owner, repo_name, pr_number)
+            return ORJSONResponse({"status": "success", "message": "PR reviewed automatically"})
+        except Exception as e:
+            print(e)
+            return ORJSONResponse({"status": "error", "message": str(e)}, status_code=500)
+
+    return ORJSONResponse({"status": "ok"})
+
+
+
+if __name__ == '__main__':
+    host = "0.0.0.0" if os.getenv("ENVIRONMENT") == "prod" else "127.0.0.1"
+    reload = os.getenv("ENVIRONMENT") != "prod"
+    uvicorn.run('main:app', host=host, port=3001, reload=reload)
